4087c0e987010b41c4b0ccd4491fe25637ca722b
Teddy <test@test.fr*>
2020-12-09 13:26:29 +0100

    Fin des exercices

---
+++ exercices/exercice2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $color = 'red';
+    $name = 'Teddy';
+    echo '<h1> Salutations '. $name .'!</h1>';
+    echo "<body style=background-color:$color;";
+
+    ?>
+</body>
+</html>
