b02610d2680efdff2ef715968d342f7ff58c5d0c
Teddy <test@test.fr*>
2020-12-09 11:53:35 +0100

    Fin de l'exercice1

---
+++ exercice1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Exercice PHP 1</h1>
+
+
+    <?php
+
+        echo '<ul>
+            <li>bananes</li>
+            <li>fraises</li>
+            <li>pommes</li>
+            <li>framboises</li>
+            <li>raisin</li>
+            </ul>';
+    ?>
+
+</body>
+</html>
