3ae64f87ae593a6d1fdcea482c0f26bc2207a256
Teddy <test@test.fr*>
2020-12-09 14:30:30 +0100

    Fin des exercices

--- exercice2/index.php
+++ exercice2/index.php
@@ -1,16 +1,24 @@
+<?php
+
+        $color = 'blue'
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body{
+            background-color : <?php echo $color  ?>;
+        }
+    </style>
 </head>
 <body>
     <?php
-    $color = 'red';
-    $name = 'Teddy';
-    echo '<h1> Salutations '. $name .'!</h1>';
-    echo "<body style=background-color:$color;";
+        $name = 'Teddy';
+        echo '<h1> Salutations '. $name .'!</h1>';
 
     ?>
 </body>
