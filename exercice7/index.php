90f222d209778f729d53f499fa830aeaf07bde24
Teddy <test@test.fr*>
2020-12-10 11:58:28 +0100

    Fin de l'exercice7

--- exercice7/index.php
+++ exercice7/index.php
@@ -4,7 +4,7 @@ $userInfo = [
     'name'=> 'Rodolphe',
     'sexe' => 'homme',
     'age'=> '36',
-    'job' =>'eleveur de tuites',
+    'job' =>'eleveur de truites',
     'where' => 'Normandie',
 ];
 ?>
@@ -22,14 +22,8 @@ $userInfo = [
    </style>
 </head>
 <body>
-
-
     <?php
-
         echo 'Aujourd\'hui, dans l\'amour est dans le pré, nous allons à la rencontre de <span>'.$userInfo['name'].'</span> jeune <span>' . $userInfo['sexe']. '</span> de <span>'. $userInfo['age']. '</span> ans. Il est <span>'.$userInfo['job'].'</span> en <span>'.$userInfo['where'].'</span>. Allons à sa rencontre...';
-
-
-
     ?>
 </body>
 </html>
