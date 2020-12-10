08815963169e7406975a833cfefaed012ae4c9a9
Teddy <test@test.fr*>
2020-12-10 11:09:34 +0100

    Fin de l'exercice6 avec corr

--- exercice6/index.php
+++ exercice6/index.php
@@ -6,18 +6,27 @@
     <title>Exercice 6</title>
 </head>
 <body>
-<ul>
-<?php
+    <ul>
+        <?php
 
-$name = ['Prenom1','Prenom2', 'Prenom3', 'Prenom4', 'Prenom5', 'Prenom6', 'Prenom7', 'Prenom8', 'Prenom9', 'Prenom10'];
-$i = 0;
+        $name = ['Prenom1','Prenom2', 'Prenom3', 'Prenom4', 'Prenom5', 'Prenom6', 'Prenom7', 'Prenom8', 'Prenom9', 'Prenom10'];
+        /*$i = 0;
 
-while($i < 10){
-    echo '<li> '.$name[$i].'</li>';
-    $i++;
-}
+        while($i < 10){
+            echo '<li> '.$name[$i].'</li>';
+            $i++;
+        }
+        */
 
-?>
-</ul>
+        //on recupere la taille de l'array avec count()
+        $arrayLength = count($name);
+
+        //on boucle avec for
+        for ($i = 0; $i < $arrayLength; $i++){
+            echo '<li>' . $name[$i] . '</li>';
+        }
+
+        ?>
+    </ul>
 </body>
 </html>
