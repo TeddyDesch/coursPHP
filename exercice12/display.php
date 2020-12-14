c9d957eeafe5a9c6c2574dc6d53048e24154fe84
Teddy <test@test.fr*>
2020-12-14 15:39:46 +0100

    Fin exos 12

--- exercice12/display.php
+++ exercice12/display.php
@@ -1,17 +1,23 @@
-<?php
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SHOP</title>
+    <title>Display</title>
 </head>
 <body>
-    <h1>SHOP</h1>
-    <?php include 'menu.php'; ?>
+
+    <?php
+        if (isset($_GET['firstname']) && isset($_GET['email'])){
+            echo 'Bonjour '. $_GET['firstname']. '<br>';
+            echo 'Ton adresse email est ' . $_GET['email']. ' !';
+        }
+        else{
+            echo 'Veuillez passer par le formulaire';
+        };
+
+    ?>
+
 </body>
 </html>
+
--- exercice12/form.php
+++ exercice12/form.php
@@ -1,18 +1,24 @@
-<?php
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SHOP</title>
+    <title>Exercice 12</title>
 </head>
 <body>
-    <h1>PROFIL</h1>
-    <?php include 'menu.php'; ?>
+
+
+    <form action="display.php" method="GET">
+
+        <input type="text" name="firstname">
+        <input type="text" name="email">
+        <input type="submit">
+    </form>
+
+
+    <?php
+
+    ?>
 
 </body>
 </html>
