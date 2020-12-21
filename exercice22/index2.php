01d85f938ffec0111aa6892891abe19b2a757595
Teddy <test@test.fr*>
2020-12-21 17:02:14 +0100

    fin ex 22

--- exercice22/index2.php
+++ exercice22/index2.php
@@ -1,25 +1,11 @@
 <?php
-
+//recuperation du nombre de visites
 $myFile = file_get_contents('compteur.txt');
-
-// //on lit le fichier et on recupere le 0
-// $visitedCount = fread($myFile, 6);
-
 // on acrement de 1
 $myFile++;
-
-
-// on déplace le curseur à la base pour ecraser le chiffre initiale
-//fseek($myFile, 0);
-
 // on ecrit le nouveau chiffre calculé
 file_put_contents('compteur.txt', $myFile);
-
-
-
 ?>
-
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -29,11 +15,7 @@ file_put_contents('compteur.txt', $myFile);
 </head>
 <body>
     <?php
-
     echo '<p> Le site a été vu '. $myFile . ' fois !!! </p>';
-
    ?>
-
-
 </body>
 </html>
